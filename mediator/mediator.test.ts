93f78bfa301b4a0e682deb6649840234f9d1259d
Rodri <rodrigomagallanes0@gmail.com>
2025-04-05 12:19:34 -0300

    Se agrego a ramiro(lo importante)

--- mediator/mediator.test.ts
+++ mediator/mediator.test.ts
@@ -3,8 +3,10 @@ import { ChatRoom, User } from './mediator';
 describe('Patrón Mediator (ChatRoom)', () => {
   it('debería enviar un mensaje a través del mediador', () => {
     const chatRoom = new ChatRoom();
-    const user1 = new User('Alice', chatRoom);
+    const user1 = new User('Ramiro', chatRoom);
+    const user2 = new User('Rodrigo', chatRoom);
 
-    user1.send('¡Hola Mundo!');
+    user1.send('Tanto tiempo Rodrigo');
+
   });
 });
